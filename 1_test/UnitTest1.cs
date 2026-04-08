bac2a1d5150741fd6a56159db3bf96736de431d2
nasrya.k <nastya.k@email.com.>
2026-04-08 23:40:00 +0500

    update tests

--- 1_test/UnitTest1.cs
+++ 1_test/UnitTest1.cs
@@ -2,39 +2,159 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
-
-namespace test_1_auth;
-
+namespace five_tests_selenium;
+//1. Структура теста — есть Setup и Teardown, авторизация вынесена в отдельный метод
+//2. Переиспользование кода — повторяющиеся блоки вынесены в отдельные методы
+//3. Нет лишних UI-действий — например, используем переход по URL вместо клика по кнопкам меню,  если этого не требуется для проверки в рамках теста
+//4. Понятные сообщения в ассертах — при падении теста сразу ясно, что пошло не так
+//5. Человекочитаемые названия тестов — проверяющий понимает, что именно тестируется
+//6. Уникальные локаторы — используются там, где это возможно
+//7. Явные или неявные ожидания — тесты не падают из-за гонки с интерфейсом   
 public class Tests
 {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    //url
+    private const string BaseUrl = "https://staff-testing.testkontur.ru/";
+    private const string NewsUrl = "https://staff-testing.testkontur.ru/news";
+    private const string CommunitiesUrl = "https://staff-testing.testkontur.ru/communities";
+
+    //данные для авторизации (скрыты)
+    private const string Login = "mila.milka11@bk.ru";
+    private const string Password = "heBdah-viwcup-rovri0";
+
+
     [SetUp]
-    public void Setup()
+    public void Setup() //метод подготовки к тесту
     {
+        driver = new ChromeDriver();
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);//неявное ожидание
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));// явное ожидание
+
+    }
+    [TearDown] 
+    public void TearDown() //событие после каждого теста
+    {
+        driver.Quit(); //закрытие браузера
+        driver.Dispose();
+
+    }
+    private void Authorize()
+    {
+        //открываем сайт
+        driver.Navigate().GoToUrl(BaseUrl);
+        //ввод логина
+        driver.FindElement(By.Id("Username")).SendKeys(Login);
+        //вести пароль
+        driver.FindElement(By.Id("Password")).SendKeys(Password);
+        //нажать кнопку войти
+        var enter = driver.FindElement(By.Name("button"));
+        enter.Click();
+        // ждем переход
+        wait.Until(ExpectedConditions.UrlToBe(NewsUrl)); //явное ожидание
+
+    }
+    private void OpenSidebar()
+    {
+    // открываем боковое меню
+        var sidebarButton = wait.Until(ExpectedConditions.ElementToBeClickable(
+        By.CssSelector("[data-tid='SidebarMenuButton']")));
+        wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("[data-tid='SidePage__root']")));
+
+        sidebarButton.Click();
+
+    // ждём появления панели меню
+        wait.Until(ExpectedConditions.ElementIsVisible(
+        By.CssSelector("[data-tid='SidePage__root']")));
     }
 
     [Test]
     public void AuthorizationTest()
-    {
-    //зайти в браузер
-    var driver = new ChromeDriver();
-    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-    driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/");
-    //вессти логин
-    var login = driver.FindElement(By.Id("Username"));
-    login.SendKeys("mila.milka11@bk.ru");
-    //вессти пароль
-    var Password = driver.FindElement(By.Id("Password"));
-    Password.SendKeys("heBdah-viwcup-rovri0");
-    //нажать кнопку войти
-    var enter = driver.FindElement(By.Name("button"));
-    enter.Click();
-    //проверить вход
-    //неявное ожидание var title = driver.FindElement
-    //явное ожидание
-    var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
-    wait.Until(ExpectedConditions.UrlToBe("https://staff-testing.testkontur.ru/news"));
-        Assert.That(driver.Title, Does.Contain("Новости"));
-    //закрыть браузер
-        driver.Quit();
+    {   // авторизация
+        Authorize();
+        //проверка авторизации
+        Assert.That(driver.Url, Is.EqualTo(NewsUrl),
+        $"Авторизация не удалась. Ожидался переход на страницу новостей ({NewsUrl}), но текущий URL: {driver.Url}");
+
     }
+    [Test]
+    public void NavigationMenuElementTest()
+    {   
+        Authorize();
+        OpenSidebar();
+        //находим пункт сообщества
+        var community = driver.FindElements(By.CssSelector("[data-tid='Community']"))
+            .First(element => element.Displayed);
+        community.Click();
+        //явное ожидание
+        wait.Until(ExpectedConditions.UrlToBe(CommunitiesUrl));
+        var pageTitleElement = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("[data-tid='Title']")));
+        Assert.That(pageTitleElement.Text, Does.Contain("Сообщества"),
+        "После перехода через боковое меню должен открыться раздел 'Сообщества', но заголовок страницы не соответствует ожидаемому.");    }
+    [Test]
+    public void SearchTest()
+    { 
+        Authorize();
+        //поле поиска открыть
+        var searchBar = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("[data-tid='SearchBar']")));
+        searchBar.Click();
+        //ввести поисковый запрос
+        var searchInput = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("[data-tid='SearchBar'] input")));
+        searchInput.SendKeys("чижевская милана игоревна");
+
+        Assert.That(searchInput.GetAttribute("value"), Does.Contain("чижевская милана игоревна"), "В поле поиска не отобразился нужный введенный поисковый запрос.");
+      
+    }
+    [Test]
+    //выход
+    public void LogoutProfile()
+    {
+        Authorize(); 
+        // открыть боковое меню
+        OpenSidebar();
+        // нажать выйти
+        var logoutButton = wait.Until(ExpectedConditions.ElementToBeClickable(
+        By.CssSelector("[data-tid='LogoutButton']")));  
+        logoutButton.Click();
+        // ожидание
+        wait.Until(ExpectedConditions.UrlContains("/Account/Logout"));
+        // проверяем сообщение об успешном выходе
+        var message = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//h3")));//  не очень хороший вариант, но не придумала лучше, неуникальный
+        Assert.That(message.Text, Does.Contain("Вы вышли из учетной записи"),"После выхода должно появиться сообщение о завершении сессии");
+        Assert.That(driver.Url, Does.Contain("/Account/Logout"),
+        "После нажатия 'Выйти' должен произойти переход на страницу выхода (/Account/Logout)");
+}
+    [Test]
+    public void LeaveComment()
+    {
+        Authorize();
+        driver.Navigate().GoToUrl(NewsUrl);
+        //рандом комментарий каждый раз
+        string expectedCommentText = $"Test comment {new Random().Next(1, 10000)}";
+        //открываем поле добавления комментария
+
+        var addCommentBlock = wait.Until(ExpectedConditions.ElementToBeClickable(
+            By.CssSelector("[data-tid='AddComment']")));
+        addCommentBlock.Click();
+    // вводим текст комментария
+        var commentTextField = wait.Until(ExpectedConditions.ElementIsVisible(
+        By.CssSelector("[placeholder='Комментировать...']")));
+        commentTextField.Clear();
+        commentTextField.SendKeys(expectedCommentText);
+    // отправляем комментарий(не получилось через нажатие на кнопку отправит, tab - выделяем отправить, enter - нажимаем отправить)
+        new OpenQA.Selenium.Interactions.Actions(driver)
+        .SendKeys(Keys.Tab)
+        .SendKeys(Keys.Enter)
+        .Perform();
+        // ждём появления комментария на странице
+
+        var newComment = wait.Until(ExpectedConditions.ElementIsVisible(
+        By.XPath($"//div[@data-tid='TextComment' and contains(text(), '{expectedCommentText}')]")));
+        // проверяем, что комментарий появился
+
+        Assert.That(newComment.Text.Trim(), Does.Contain(expectedCommentText),
+        $"Ожидался комментарий '{expectedCommentText}', " +
+        $"но появился: '{newComment.Text}'");
+}
 }
